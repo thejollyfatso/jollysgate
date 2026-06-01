2a00e1211ebf95439e5cc31940053814a019253d
thejollyfatso <rafael.r.deleon@gmail.com>
2026-06-01 13:30:58 -0700

    initial scaffold from nf4lm 305883f

---
+++ server/main.py
@@ -0,0 +1,19 @@
+import os
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI(title="nf4lm")
+
+
+@app.get("/health")
+def health():
+    return {"ok": True, "app": "nf4lm"}
+
+
+# TODO: wire up routers once implemented
+# from server.routers import auth, recipes, shopping, events
+# app.include_router(auth.router, prefix="/auth")
+# app.include_router(recipes.router, prefix="/recipes")
+# app.include_router(shopping.router, prefix="/shopping")
+# app.include_router(events.router)
