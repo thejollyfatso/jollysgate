b1b46dd3c99518b63199a7dfa7664153f5139c95
thejollyfatso <rafael.r.deleon@gmail.com>
2026-06-01 20:01:45 -0700

    removed react router dom for federation negotiation

--- client/vite.config.js
+++ client/vite.config.js
@@ -18,7 +18,6 @@ export default defineConfig({
       shared: {
         react: { singleton: true },
         "react-dom": { singleton: true },
-        "react-router-dom": { singleton: true, requiredVersion: "^7.0.0" },
       },
     }),
     VitePWA({
